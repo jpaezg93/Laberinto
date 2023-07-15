8bc571a45ded012e37729524964cbebf77e1554a
Juan Paez <jpaezg93@gmail.com>
2023-07-14 19:30:19 -0500

    Agregamos bucle infinito que imprime las teclas y se termina hasta que se oprime la tecla ↑ indicada como UP

--- proy_int.py
+++ proy_int.py
@@ -1,2 +1,11 @@
-nombre=input('Hola! Cual es tu nombre?: ')
+from readchar import readkey, key
+
+nombre = input('Hola! Cual es tu nombre?: ')
 print('Que tal', nombre+'?','Bienvenido al juego.',sep=" " )
+
+while True:
+  k = readkey()
+  print(k)
+  if k == key.UP:
+    break
+
