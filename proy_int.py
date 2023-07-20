fe248fdf7afa8547e72ce05321515dbbe8d22356
Juan Paez <jpaezg93@gmail.com>
2023-07-20 11:44:04 -0500

    añadimos funcion para limpiar la pantalla cuando se presiona n o cuando se presionan hasta 50 teclas

--- proy_int.py
+++ proy_int.py
@@ -1,11 +1,24 @@
 from readchar import readkey, key
+import os
 
 nombre = input('Hola! Cual es tu nombre?: ')
-print('Que tal', nombre+'?','Bienvenido al juego.',sep=" " )
+print('Que tal', nombre+'?','Bienvenido a nuestro laberinto!\nHaz tu mejor esfuerzo para salir!',sep=" " )
 
+def clear_p(k,c1):
+  if k == 'n' or c1>=50:
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print(c1)
+
+c1=0
 while True:
   k = readkey()
+  c1+=1
   print(k)
-  if k == key.UP:
+  if k == key.ENTER:
     break
-
+  if c1<50:
+    clear_p(k,c1)
+    continue
+  elif c1>=50:
+    clear_p(k, c1)
+    c1=0
